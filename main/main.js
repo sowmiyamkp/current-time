aac4552ae89daf5c4a26fff86841b382070db8f7
sowmiyamkp <sowmiyamkp1305@gmail.com>
2023-03-24 16:35:19 -0700

    add current time

---
+++ main/main.js
@@ -0,0 +1,37 @@
+// var time=new Date();
+// let timer=time.getMonth();
+// console.log(timer);
+
+
+
+var current=new Date();
+var hour=current.getHours();
+if(hour<12)
+{
+	console.log("HapPy morning")
+}
+else if(12<=hour&&hour<17){
+	console.log("Afternoon");
+}
+
+else if(17<=hour&&hour<20)
+{
+	console.log("happy evening")
+}
+else{
+	console.log("good night");
+}
+
+
+
+
+
+
+
+
+
+
+	
+
+	
+	
